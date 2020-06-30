ca7999067ef1c6cb533c01a4a7d0d9a8c6f2543a
Ben Le <lebenjamin77@yahoo.com>
2020-06-30 02:08:42 -0600

    adding schema

--- employeeinfo_db/schema.sql
+++ employeeinfo_db/schema.sql
@@ -1,27 +1,29 @@
-CREATE DATABASE employeeinfo_db
+DROP DATABASE IF EXISTS employeeinfo_db;
+CREATE DATABASE employeeinfo_db;
+
 
 USE employeeinfo_db;
 
 CREATE TABLE department (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(40) NOT NULL
-
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(40) NOT NULL,
-        salary DECIMAL (12,2),
-        department_id INT NOT NULL,
-        FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL (12, 2),
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        first_name VARCHAR(40) NOT NULL,
-        last_name VARCHAR(40) NOT NULL,
-        role_id INT NOT NULL,
-        FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-        manager_id INT AUTO_INCREMENT PRIMARY KEYE,
-        FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    
 );
