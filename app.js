1da73107d82623f4497230e6970999f5de389982
Ben Le <lebenjamin77@yahoo.com>
2020-07-06 15:01:48 -0600

    adding inquirer prompt

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const { inherits } = require("util");
 
 
 
@@ -15,20 +16,67 @@ const connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-
+    
 });
 
-function afterConnection() {
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        console.log(res);
-        console.table(res);
-        connection.end();
+//function afterConnection() {
+  //  connection.query("SELECT * FROM products", function(err, res) {
+    //    if (err) throw err;
+      //  console.log(res);
+        //console.table(res);
+        //connection.end();
 
-    });
+    //});
 
+//}
+
+connection.connect();
+
+init();
+
+function searchDB() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View All Departments",
+                "View All Roles",
+                "View All Employees",
+                "Add Department",
+                "Add Role",
+                "Add Employee",
+                "Update Employee Role",
+                "Exit"
+            ]
+
+        }).then((response) => {
+            switch (response.action) {
+                case "View All Departments":
+                    viewAllDepartments();
+                    break;
+                case "View All Roles":
+                    viewAllRoles();
+                    break;
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                default :
+                    exitApp();
+                    break;
+            }
+        })
 }
-
-
-
-
