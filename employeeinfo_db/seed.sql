18b04babcfb2e3ef5101f0a5c70eb17ccc07a5e9
Ben Le <lebenjamin77@yahoo.com>
2020-07-06 16:29:48 -0600

    editing the seed to be more condensed

--- employeeinfo_db/seed.sql
+++ employeeinfo_db/seed.sql
@@ -1,14 +1,18 @@
-INSERT INTO department (department_name) VALUES ("General Management");
-INSERT INTO department (department_name) VALUES ("Human Resources");
-INSERT INTO department (department_name) VALUES ("Finance");
-INSERT INTO department (department_name) VALUES ("Sales");
+INSERT INTO department (department_name)
+VALUES ("General Management"),("Human Resources"),("Finance"),("Sales");
 
-INSERT INTO role (title, salary, department_id) VALUES ("Manager", 5000000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("Office Coordinator", 60000, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Accountant", 95000, 3);
-INSERT INTO role (title, salary, department_id) VALUES ("Sales Specialist", 100000, 4);
+--INSERT INTO department (department_name) VALUES ("General Management");
+--INSERT INTO department (department_name) VALUES ("Human Resources");
+--INSERT INTO department (department_name) VALUES ("Finance");
+--INSERT INTO department (department_name) VALUES ("Sales");
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Ben", "Ten", 1, NULL);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Robert", "Downey Jr.", 2, NULL);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Dwayne", "Johnson", 3, NULL);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Christian", "Bale", 4, NULL);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 5000000, 1),("Office Coordinator", 60000, 2),("Accountant", 95000, 3),("Sales Specialist", 100000, 4);
+--INSERT INTO role (title, salary, department_id) VALUES ("Manager", 5000000, 1);
+--INSERT INTO role (title, salary, department_id) VALUES ("Office Coordinator", 60000, 2);
+--INSERT INTO role (title, salary, department_id) VALUES ("Accountant", 95000, 3);
+--INSERT INTO role (title, salary, department_id) VALUES ("Sales Specialist", 100000, 4);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("LeVALUES ("Ben", "Ten", 1, 1),("Robert", "Downey Jr.", 2, NULL),("Andy", "Serkis", 3, NULL),("April", "Oneal", 4, NULL);
